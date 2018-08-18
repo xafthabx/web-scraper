e1c373ee904c6d203c0a7c6e86928d632a0de1ab
Afthab Abubacker <afthab.abuabcker@team.telstra.com>
2018-08-18 13:28:29 +1000

    added work credit

--- scraper.py
+++ scraper.py
@@ -23,7 +23,7 @@ for i in range(543):
 
     page_result = soup.find_all('div', attrs={'class': 'result'})
 
-    all_works = ["artist_name, piece_title, piece_ref, piece_url"]
+    all_works = ["artist_name, piece_title, work_credit, piece_ref, piece_url, page_number"]
     
     for result in page_result:
         artist_name = result.find('span', attrs={'class': 'artist'}).text.encode('utf-8').strip()
@@ -44,7 +44,14 @@ for i in range(543):
             piece_year = soup2.find('article', attrs={'class': 'collection_work'}).find('div', attrs={'class': 'hero'}).find('div', attrs={'class': 'left'}).find('div').p.get_text(strip=True, separator=" ").encode('utf-8').strip()
         except:
             piece_year = "NOT FOUND"
-        work = "\"{0}\",\"{1}\",\"{2}\",\"{3}\",{4}, {5}".format(artist_name, piece_title, work_origin, piece_year, piece_ref, piece_url)
+        
+        try:
+            work_credit_raw = soup2.find('div', attrs={'class': 'credit'}).p.text.encode('utf-8').strip()
+            work_credit = re.sub ('\s+', ' ', work_credit_raw)
+        except:
+            work_credit = "NOT FOUND"
+
+        work = "\"{0}\",\"{1}\",\"{2}\",\"{3}\",\"{4}\",{5}, {6}, {7}".format(artist_name, piece_title, work_origin, piece_year, work_credit, piece_ref, piece_url, i)
         all_works.append(work)
         print work
         print >> output_file, work
